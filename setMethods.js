16ee05cf510e9672d2e4cdeabdddd0c94c595a62
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2022-01-04 00:03:12 +0530

    Create setMethods.js

---
+++ setMethods.js
@@ -0,0 +1,21 @@
+// 1. setDate()
+let d = new Date();
+d.setDate(20);
+console.log(d);
+
+// 2. setFullYear()
+let d = new Date();
+d.setFullYear(2016);
+console.log(d);
+
+// 3. setHours()
+let d = new Date();
+d.setHours(15);
+console.log(d);
+
+// 4. setMilliseconds()
+let d = new Date();
+d.setMilliseconds(548);
+console.log(d);
+
+// Similarly, you can use the setMinutes(), setMonth(), and setSeconds() methods to set the minutes (0-59), month (0-11), and seconds (0-59) respectively for a specified date according to local time.
