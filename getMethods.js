c0eba2df02af4a3a7a0da690e810c1c405b980bc
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2022-01-04 00:01:23 +0530

    Create getMethods.js

---
+++ getMethods.js
@@ -0,0 +1,35 @@
+// 1. getFullYear()
+let d = new Date();
+console.log(d.getFullYear());
+
+// 2. getMonth()
+let d = new Date();
+console.log(d.getMonth());
+
+// 3. getDate()
+let d = new Date();
+console.log(d.getDate());
+
+// 4. getHours()
+let d = new Date();
+console.log(d.getHours());
+
+// 5. getMinutes()
+let d = new Date();
+console.log(d.getMinutes());
+
+// 6. getSeconds()
+let d = new Date();
+console.log(d.getSeconds());
+
+// 7. getMilliseconds()
+let d = new Date();
+console.log(d.getMilliseconds());
+
+// 8. getTime()
+let d = new Date();
+console.log(d.getTime());
+
+// 9. getDay()
+let d = new Date();
+console.log(d.getDay());
